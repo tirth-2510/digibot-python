803e7264518e79ecc3301f89c3aa2f745635d7b7
tirth-2510 <iamtirth2510@gmail.com>
2025-07-17 15:07:50 +0530

    change: only chat endpoint

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 import os
-import io
-from fastapi import Body, FastAPI, HTTPException, File, Form, UploadFile
+from fastapi import Body, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
-from pymilvus import MilvusClient
 from langchain_milvus import Zilliz
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage
-from langchain_community.document_loaders import PDFPlumberLoader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from pymongo import MongoClient
-from bson.objectid import ObjectId
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,45 +20,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-mongo_uri = os.getenv("MONGODB_URI")
-client = MongoClient(mongo_uri)
-db = client["digibot"]
-ud_db = db["user_details"]
-
 llm = ChatGroq(api_key=os.getenv("GROQ_API_KEY"), temperature=0, model="llama-3.3-70b-versatile")
 
 embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004", google_api_key=os.getenv("GOOGLE_API_KEY"))
-
-def extract_pdf_text(file) -> str:
-    text = ""
-    pdf_reader = PDFPlumberLoader(file_path=file).load()
-    for page in pdf_reader:
-        text += page.page_content or ""
-    return text
-
-def get_text_chunks(text: str):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    return splitter.split_text(text)
-
-def generate_file_ids(chunks:list[str], file_name:str):
-    file_id = [f"{file_name}_{i}" for i in range(len(chunks))]
-    return file_id
-
-def create_vector_store(chunks: list[str], document_id: str, file_id: list[str]):
-    if document_id is None:
-        raise HTTPException(status_code=400, detail="Document ID is required")
-    else:
-        collection_name = f"id_{document_id}"
-        Zilliz.from_texts(
-            texts=chunks,
-            embedding=embeddings,
-            connection_args={"uri": os.getenv("ZILLIZ_URI_ENDPOINT"), "token":os.getenv("ZILLIZ_TOKEN")},
-            collection_name=collection_name,
-            ids=file_id,
-            index_params={"index_type": "IVF_PQ", "metric_type": "COSINE"},
-            drop_old=False,
-        )
-        return({"message": "Vector store created successfully."})
 
 async def generate_chat_response(document_id: str, bot_name: str, user_input: str):
     # Search for relevant document context
@@ -124,76 +82,3 @@ async def chat(data: dict = Body(...)):
     user_input = data.get("user_input")
 
     return StreamingResponse(generate_chat_response(document_id, bot_name, user_input), media_type="text/plain")
-    
-@app.post("/upload")
-async def upload_pdf(file: UploadFile = File(...), document_id: str = Form(...)):
-    try:
-        file_name = file.filename
-        user_data = ud_db.find_one({"_id": ObjectId(document_id)})
-        if not user_data:
-            raise HTTPException(status_code=404, detail="User not found")
-        if file.filename in user_data["user_files"]:
-            pass
-        else:
-            updated_data = ud_db.find_one_and_update(
-                {"_id": ObjectId(document_id)},
-                {"$addToSet": {"user_files": file_name}},
-            )
-            if updated_data:
-                content = await file.read()
-                pdf_text = extract_pdf_text(io.BytesIO(content))
-                text_chunks = get_text_chunks(pdf_text)
-                ud_db.find_one_and_update(
-                    {"_id": ObjectId(document_id)},
-                    {"$push": {"file_ids": len(text_chunks)}}
-                )
-                file_ids = generate_file_ids(text_chunks, file_name)
-                create_vector_store(text_chunks, document_id,file_ids)
-            
-        return {"message": "Operation completed."}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error processing PDF: {str(e)}")
-
-@app.delete("/delete")
-async def delete_file(data: dict = Body(...)):
-    document_id = data.get("document_id")
-    file_name = data.get("file_name")
-    try:
-        result = ud_db.find_one({"_id": ObjectId(document_id)})
-        file_index = result["user_files"].index(file_name)
-        file_id = result["file_ids"][file_index]
-        unique_id = [f"{file_name}_{i}" for i in range(file_id)]
-        vector_store = Zilliz(
-            collection_name=f"id_{document_id}",
-            connection_args={"uri": os.getenv("ZILLIZ_URI_ENDPOINT"), "token":os.getenv("ZILLIZ_TOKEN")},
-            embedding_function=embeddings
-        )
-        vector_store.delete(unique_id)
-
-        # Remove File name
-        ud_db.find_one_and_update(
-            {"_id": ObjectId(document_id)},
-            {"$pull": {"user_files": file_name}},
-        )
-        # Set file_id to null
-        ud_db.find_one_and_update(
-            {"_id": ObjectId(document_id)},
-            {"$unset": {f"file_ids.{file_index}": 1}},
-        )
-        # remove null value
-        ud_db.find_one_and_update(
-            {"_id": ObjectId(document_id)},
-            {"$pull": {f"file_ids":None }},
-        )
-        client = MilvusClient(uri=os.getenv("ZILLIZ_URI_ENDPOINT"), token=os.getenv("ZILLIZ_TOKEN"))
-        collection = client.get_collection_stats(f"id_{document_id}")
-        if collection["row_count"] == 0:
-            client.drop_collection(f"id_{document_id}")
-        
-        return {"message": "File deleted successfully."} 
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error deleting file: {str(e)}")
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app:app", host="localhost", port=8000, reload=True)
